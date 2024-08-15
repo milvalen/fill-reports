c675a1b450eb59c0957ad7164ac9ad0f7f4fbc2b
milvalen <artemyerkov@gmail.com>
2024-08-15 03:46:37 +0300

    installer check pip

--- install.bat
+++ install.bat
@@ -7,10 +7,15 @@ if %errorlevel% neq 0 (
     powershell -Command "Start-Process msiexec -ArgumentList '/i https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1' -Wait"
 )
 
-python -m ensurepip --upgrade
+python -m ensurepip --upgrade >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Установка pip...
+    python -m ensurepip --default-pip
+)
 
-pip install --upgrade pip
-pip install python-docx ezodf
+python -m pip install --upgrade pip
+
+python -m pip install python-docx ezodf
 
 echo Установка завершена!
 pause
